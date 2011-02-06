cea27f57d8d0898031f83a3679cde0d352c7873a
chiwi <chiwi@1215cf96-3890-4680-975f-d2b89ea09188>
2011-02-06 20:33:30 +0000

    agregue campos al location, latitud y longitud.
    git-svn-id: https://subversion.assembla.com/svn/uadepfi2011@8 1215cf96-3890-4680-975f-d2b89ea09188

--- core/src/main/java/com/uade/pfi/core/Location.java
+++ core/src/main/java/com/uade/pfi/core/Location.java
@@ -2,4 +2,28 @@ package com.uade.pfi.core;
 
 public class Location {
 
+	private int latitude;
+	private int longitude;
+
+	public Location(int latitude, int longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public Location() {
+	}
+	
+	public void setLatitude(int latitude) {
+		this.latitude = latitude;
+	}
+	public void setLongitude(int longitude) {
+		this.longitude = longitude;
+	}
+	public int getLatitude() {
+		return latitude;
+	}
+	public int getLongitude() {
+		return longitude;
+	}
+	
 }
