3dcaffde695815b1b21d4bc8559513e370d3fd82
chiwi <chiwi@1215cf96-3890-4680-975f-d2b89ea09188>
2011-02-06 19:31:32 +0000

    primera version de la interfaz y de la implementacion.
    git-svn-id: https://subversion.assembla.com/svn/uadepfi2011@5 1215cf96-3890-4680-975f-d2b89ea09188

---
+++ core/src/main/java/com/uade/pfi/core/service/PublicTransportTrackerService.java
@@ -0,0 +1,12 @@
+package com.uade.pfi.core.service;
+
+import java.util.List;
+
+import com.uade.pfi.core.Location;
+
+public interface PublicTransportTrackerService {
+
+	List<Location> retrieveLocations();
+	
+	
+}
---
+++ core/src/main/java/com/uade/pfi/core/service/PublicTransportTrackerServiceImpl.java
@@ -0,0 +1,16 @@
+package com.uade.pfi.core.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.uade.pfi.core.Location;
+
+public class PublicTransportTrackerServiceImpl implements
+		PublicTransportTrackerService {
+
+	public List<Location> retrieveLocations() {
+		//TODO refactorizar esto. Debe devolver la lista posta.
+		return new ArrayList<Location>();
+	}
+
+}
