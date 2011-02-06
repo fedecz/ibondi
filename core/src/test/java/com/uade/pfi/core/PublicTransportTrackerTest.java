09a6bb88cbadad14af2b4367f1da312a1df6fab5
chiwi <chiwi@1215cf96-3890-4680-975f-d2b89ea09188>
2011-02-06 20:35:22 +0000

    Mas tests.
    git-svn-id: https://subversion.assembla.com/svn/uadepfi2011@11 1215cf96-3890-4680-975f-d2b89ea09188

--- core/src/test/java/com/uade/pfi/core/PublicTransportTrackerTest.java
+++ core/src/test/java/com/uade/pfi/core/PublicTransportTrackerTest.java
@@ -13,18 +13,60 @@ import com.uade.pfi.core.service.PublicTransportTrackerServiceImpl;
 public class PublicTransportTrackerTest {
 
 	PublicTransportTrackerService service;
-	
+
 	@Before
 	public void setup(){
 		service = new PublicTransportTrackerServiceImpl();
 	}
-	
-	
+
 	@Test
 	public void elServicioDebeDevolverListasPosicionesDeLosTransportes(){
-		List<Location> locations=service.retrieveLocations();
-		
+		List<TransportLocation> locations=service.retrieveLocations();
+
 		assertNotNull(locations);
 	}
+
+	@Test
+	public void elServicioDebeDejarUpdetearMiPosicionEnElMedioDeTransporte(){
+		TransportLocation location = new TransportLocation(2,3,"60");
+
+		service.updatePosition(location);
+	}
+
+	@Test
+	public void elServicioDebeGuardarLaPosicionYVerlaEnElMapa(){
+		List<TransportLocation> locations=service.retrieveLocations();
+		assertNotNull(locations);
+		assertEquals(0, locations.size());
+		
+		TransportLocation location = new TransportLocation(2,3,"60");
+		service.updatePosition(location);
+		locations=service.retrieveLocations();
+		
+		assertEquals(1, locations.size());
+		TransportLocation newLocation = locations.get(0);
+		assertEquals(2, newLocation.getLocation().getLatitude());
+		assertEquals(3, newLocation.getLocation().getLongitude());
+		assertEquals("60", newLocation.getName());
+	}
 	
+	@Test
+	public void elServicioTieneQueGenerarUnPromedioDeLasPosicionesAnteElMismoTransporte(){
+		TransportLocation location1 = new TransportLocation(2,3,"60");
+		TransportLocation location2 = new TransportLocation(4,6,"60");
+		TransportLocation location3 = new TransportLocation(1,2,"59");
+		
+		service.updatePosition(location1);
+		service.updatePosition(location2);
+		service.updatePosition(location3);
+		List<TransportLocation> locations = service.retrieveLocations();
+		
+		assertNotNull(locations);
+		assertEquals(2, locations.size());
+	}
+
+
+
+
+
 }
