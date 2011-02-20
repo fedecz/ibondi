0c2184b36f9e9bd6b5b8fc6e5e3b76f7c78b0b53
chiwi <chiwi@1215cf96-3890-4680-975f-d2b89ea09188>
2011-02-20 09:46:42 +0000

    Share project "core-dto" into "https://subversion.assembla.com/svn/uadepfi2011"
    git-svn-id: https://subversion.assembla.com/svn/uadepfi2011@28 1215cf96-3890-4680-975f-d2b89ea09188

---
+++ core-dto/src/main/java/com/uade/pfi/core/dto/Location.java
@@ -0,0 +1,29 @@
+package com.uade.pfi.core.dto;
+
+public class Location {
+
+	private Float latitude;
+	private Float longitude;
+
+	public Location(Float latitude, Float longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public Location() {
+	}
+	
+	public void setLatitude(Float latitude) {
+		this.latitude = latitude;
+	}
+	public void setLongitude(Float longitude) {
+		this.longitude = longitude;
+	}
+	public Float getLatitude() {
+		return latitude;
+	}
+	public Float getLongitude() {
+		return longitude;
+	}
+	
+}
---
+++ core-dto/src/main/java/com/uade/pfi/core/dto/TransportLocation.java
@@ -0,0 +1,28 @@
+package com.uade.pfi.core.dto;
+
+public class TransportLocation {
+	
+	private Location 	location;
+	private String 		name;
+	
+	public TransportLocation() {
+	}
+	
+	public TransportLocation(Float latitude, Float longitude, String name) {
+		this.location = new Location(latitude,longitude);
+		this.name = name;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
