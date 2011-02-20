347107128f66b94ec88dc3ab93a77e1eeb8bb0ce
chiwi <chiwi@1215cf96-3890-4680-975f-d2b89ea09188>
2011-02-20 09:59:52 +0000

    primera version del servlet con JSON.
    git-svn-id: https://subversion.assembla.com/svn/uadepfi2011@33 1215cf96-3890-4680-975f-d2b89ea09188

---
+++ web/src/main/java/com/uade/pfi/web/controller/MobileInterface.java
@@ -0,0 +1,30 @@
+package com.uade.pfi.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.uade.pfi.core.dto.TransportLocation;
+import com.uade.pfi.core.service.PublicTransportTrackerService;
+
+@Controller
+public class MobileInterface {
+	@Autowired
+	private PublicTransportTrackerService service;
+
+	@RequestMapping(value="/location/get.json")
+	public @ResponseBody Integer count(){
+		System.out.println("get accessed");
+		return service.retrieveLocations().size();
+	}
+	
+	@RequestMapping("/location/post.json")
+	public @ResponseBody Boolean postLocation(@RequestBody TransportLocation location){
+		System.out.println("post accessed");
+		service.updatePosition(location);
+		return true;
+	}
+	
+}
