47317d891a5e92e589efb9e2ea00c893de6d2ef4
chiwi <chiwi@1215cf96-3890-4680-975f-d2b89ea09188>
2011-02-20 09:50:01 +0000

    Share project "androidApp" into "https://subversion.assembla.com/svn/uadepfi2011"
    git-svn-id: https://subversion.assembla.com/svn/uadepfi2011@30 1215cf96-3890-4680-975f-d2b89ea09188

---
+++ androidApp/src/com/uade/pfi/androidapp/activity/TransportMe.java
@@ -0,0 +1,68 @@
+package com.uade.pfi.androidapp.activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.uade.pfi.androidApp.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class TransportMe extends Activity {
+	private String BASE_URL = "http://chiwi.homelinux.com:8080";
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		Button startButton = (Button) findViewById(R.id.startButton);
+		startButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				TransportMe.this.startService(new Intent("TRANSPORT_ME_SERVICE"));
+			}
+		});
+		Button stopButton = (Button) findViewById(R.id.stopButton);
+		stopButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				TransportMe.this.stopService(new Intent("TRANSPORT_ME_SERVICE"));
+			}
+		});
+		
+		Button getButton = (Button) findViewById(R.id.getButton);
+		getButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				List<MediaType> acceptableMediaTypes = new ArrayList<MediaType>();
+				acceptableMediaTypes.add(MediaType.APPLICATION_JSON);
+
+				HttpHeaders requestHeaders = new HttpHeaders();
+				requestHeaders.setAccept(acceptableMediaTypes);
+
+				HttpEntity<?> requestEntity = new HttpEntity<Object>(requestHeaders);
+			    String url = BASE_URL + "/web/location/get.json";
+			    RestTemplate restTemplate =  new RestTemplate();
+			    ResponseEntity<Integer> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, Integer.class);
+			    Integer result = responseEntity.getBody();
+			    Toast.makeText(getApplicationContext(),result+"", Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
+}
---
+++ androidApp/src/com/uade/pfi/androidapp/service/TransportMeService.java
@@ -0,0 +1,95 @@
+package com.uade.pfi.androidapp.service;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.GpsStatus;
+import android.location.GpsStatus.Listener;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import com.uade.pfi.core.dto.TransportLocation;
+
+public class TransportMeService extends Service {
+	private String BASE_URL = "http://chiwi.homelinux.com:8080";
+	private LocationManager locationManager;
+	private LocationListener locationListener;
+	private Listener listener = new Listener() {
+		@Override
+		public void onGpsStatusChanged(int event) {
+			String msg =null;
+			switch(event){
+			case GpsStatus.GPS_EVENT_STARTED: msg = "GPS Started"; break;
+			case GpsStatus.GPS_EVENT_STOPPED: msg = "GPS Stopped";break;
+			case GpsStatus.GPS_EVENT_FIRST_FIX: msg = "GPS First Fix";
+			}
+			if(msg!=null)
+				Toast.makeText(getApplicationContext(),msg , Toast.LENGTH_SHORT).show();
+		}
+	};
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+		// Define a listener that responds to location updates
+		locationListener = new LocationListener() {
+			public void onLocationChanged(Location location) {
+				// Called when a new location is found by the network location provider.
+				makeUseOfNewLocation(location);
+			}
+
+			public void onProviderEnabled(String provider) {}
+
+			public void onProviderDisabled(String provider) {}
+
+			public void onStatusChanged(String provider, int status,Bundle extras) {}
+		};
+
+		// Register the listener with the Location Manager to receive location updates
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 30, locationListener);
+		locationManager.addGpsStatusListener(listener);
+		Toast.makeText(getApplicationContext(), "Service Started", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+
+	private void makeUseOfNewLocation(Location newLocation) {
+		
+		Float latitude = new Float(newLocation.getLatitude());
+		Float longitud = new Float(newLocation.getLongitude());
+		TransportLocation location = new TransportLocation(latitude, longitud, "chiwi");
+		HttpHeaders requestHeaders = new HttpHeaders();
+		requestHeaders.setContentType(new MediaType("application","json"));
+
+		HttpEntity<TransportLocation> requestEntity = new HttpEntity<TransportLocation>(location, requestHeaders);
+		RestTemplate rest = new RestTemplate();
+		ResponseEntity<Boolean> exchange = rest.exchange(BASE_URL + "/web/location/post.json", HttpMethod.POST, requestEntity, Boolean.class);
+		Toast.makeText(getApplicationContext(), "Location sent: " + exchange.getBody(), Toast.LENGTH_SHORT).show();
+	}	
+
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		locationManager.removeUpdates(locationListener);
+		Toast.makeText(getApplicationContext(), "Service Stopped", Toast.LENGTH_SHORT).show();
+	}
+
+}
