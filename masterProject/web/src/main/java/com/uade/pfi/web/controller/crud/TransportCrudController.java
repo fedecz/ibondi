fd974fffdd3a70a7137ccd19ab174c790433473b
Sergio Magnacco <smagnacco@gmail.com>
2012-09-28 15:00:58 -0300

    arreglando el controler puto

--- masterProject/web/src/main/java/com/uade/pfi/web/controller/crud/TransportCrudController.java
+++ masterProject/web/src/main/java/com/uade/pfi/web/controller/crud/TransportCrudController.java
@@ -1,14 +1,14 @@
 package com.uade.pfi.web.controller.crud;
 
-import java.util.Collection;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.uade.pfi.core.beans.Transport;
@@ -21,30 +21,26 @@ import com.uade.pfi.core.service.TransportCrudService;
  * 
  */
 @Controller
+@SessionAttributes
 public class TransportCrudController {
 
 	@Autowired
 	private TransportCrudService service;
 
-	@ModelAttribute public Transport getTransport(@RequestParam(required=false) Integer id) {
-		return (id != null ? service.getTransportBy(Integer.toString(id)) : new Transport());
-	}
-
 	@RequestMapping("/transport/transportCrudRequestForm.htm")
 	public ModelAndView showEmptyForm() {
-		return new ModelAndView("/WEB-INF/jsp/transportCrudRequestForm.jsp");
+		return new ModelAndView("transportCrudRequestForm");
 	}
 
-	@RequestMapping(value="/transport/form.htm", method=RequestMethod.POST)
-	public String form(Transport transport, Model model) {
+	@RequestMapping(value="/transport/addTransport.htm", method=RequestMethod.POST)
+	public ModelAndView addContact(@ModelAttribute("transport") Transport transport) {
 		Transport result = service.add(transport);
-		model.addAttribute("statusMessageKey", "Success");
-		model.addAttribute("createdTransport", result);
-		return "/WEB-INF/jsp/showNewTransport.jsp";
+		return showTransportList();
 	}
 
-	@RequestMapping(value="/transport/search.htm")
-	public @ModelAttribute("transport") Collection<Transport> search() {
-		return service.getAllTransports();
+	@RequestMapping(value="/transport/showTransportList.htm")
+	public ModelAndView showTransportList() {
+		List<Transport> transportList = service.getAllTransports();
+		return new ModelAndView("showTransportList", "transportList", transportList);
 	}
 }
