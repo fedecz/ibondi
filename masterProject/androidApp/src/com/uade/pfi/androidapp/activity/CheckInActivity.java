72124d3c73ef2751941a469d4e03c936af5d8b92
Sergio Magnacco <smagnacco@gmail.com>
2012-10-30 00:48:42 -0300

    Moviendo selectTransport a checkin, falta hacerlo dinamico, baby steps

--- masterProject/androidApp/src/com/uade/pfi/androidapp/activity/CheckInActivity.java
+++ masterProject/androidApp/src/com/uade/pfi/androidapp/activity/CheckInActivity.java
@@ -7,7 +7,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.uade.pfi.androidapp.service.TransportMeService;
 import com.uadepfi.android.R;
@@ -24,10 +24,20 @@ public class CheckInActivity extends Activity{
 	}
 	
 	public void checkIn(View v){
-		EditText transportIdTextbox = (EditText) findViewById(R.id.checkInTransportIdTxtBox);
-		final String transportId = transportIdTextbox.getText().toString();
+		Spinner transportTypeSpinner = (Spinner) findViewById(R.id.transportType_spinner);
+		Spinner lineSpinner = (Spinner) findViewById(R.id.line_spinner);
+		Spinner branchSpinner = (Spinner) findViewById(R.id.branch_spinner);
+		Spinner headingSpinner = (Spinner) findViewById(R.id.headingType_spinner);
+		String transportType = (String) transportTypeSpinner.getSelectedItem();
+		String line = (String) lineSpinner.getSelectedItem();
+		String branch = (String) branchSpinner.getSelectedItem();
+		String heading = (String) headingSpinner.getSelectedItem();
+		
 		Intent intent = new Intent(this,TransportMeService.class);
-		intent.putExtra("transportId", transportId);
+		intent.putExtra("transportType", transportType);
+		intent.putExtra("line", line);
+		intent.putExtra("branch", branch);
+		intent.putExtra("heading", heading);
 		CheckInActivity.this.startService(intent);
 		CheckInActivity.this.finish();
 	}
